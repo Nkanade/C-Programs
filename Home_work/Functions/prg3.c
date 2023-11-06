1f0c46af4d0923617c93f2721bfda3648af530ca
Nkanade <35738508+Nkanade@users.noreply.github.com>
2023-11-06 14:54:19 +0530

    Update prg3.c

--- Home_work/Functions/prg3.c
+++ Home_work/Functions/prg3.c
@@ -1,11 +1,11 @@
-//Q5. Swap Two Numbers With Third Variable
+/*Q5. Write a function to swap the values of two variables using a thrid variable .*/
 #include<stdio.h>
 
-void swap();
+void swap();                    // Here We Declare The Swap Function.
 
 int main()
 {
-    swap();
+    swap();                    // Here We Call The Function.
 }
 
 void swap()
@@ -13,12 +13,15 @@ void swap()
     int num1,num2,temp;
     printf("Enter The Number1 :");
     scanf("%d",&num1);
+    
     printf("Enter The Number2 :");
     scanf("%d",&num2);
-    temp=num1;
+    
+    temp=num1;                            // Here Is The Swaping Logic.
     num1=num2;
     num2=temp;
-    printf("\n%d",num1);
+    
+    printf("\n%d",num1);                   // Here We Print the Num1 and Num2.
     printf("\n%d",num2);
 
 }
